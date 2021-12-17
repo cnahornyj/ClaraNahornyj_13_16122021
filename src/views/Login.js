752d5f5c26db35ca7353ff761b197e67b7f9c84e
Clara <c.nahornyj@hotmail.fr>
2021-12-17 15:39:33 +0100

    Graphisme page login ok

--- src/views/Login.js
+++ src/views/Login.js
@@ -7,7 +7,7 @@ class Login extends Component {
     return (
       <div>
         <Header />
-        <main className="main bg-dark">
+        <main className="main bg-dark-blue">
           <section className="sign-in-content">
             <i className="fa fa-user-circle sign-in-icon"></i>
             <h1>Sign In</h1>
