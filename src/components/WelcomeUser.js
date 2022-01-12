4de1fb7bf853e754012cc4684a707a5a03fa8a22
Clara <c.nahornyj@hotmail.fr>
2022-01-12 13:01:23 +0100

    Passage d'une fonction au bouton permettant d'éditer le nom de l'utilisateur

--- src/components/WelcomeUser.js
+++ src/components/WelcomeUser.js
@@ -1,14 +1,20 @@
-import React, { Component } from 'react';
+function WelcomeUser(props) {
 
-class WelcomeUser extends Component {
-    render() {
-        return (
-            <div className='header'>
-                <h1>Welcome back <br/>{this.props.userName}</h1>
-                <button className='edit-button'>Edit name</button>
-            </div>
-        );
-    }
+
+// onClick affichage du composant EditUserName
+const editName = () => {
+  console.log("Click!")                                                                                                                                                                                            
+}
+
+  return (
+    <div className="header">
+      <h1>
+        Welcome back <br />
+        {props.userName}
+      </h1>
+      <button className="edit-button" onClick={editName}>Edit name</button>
+    </div>
+  );
 }
 
 export default WelcomeUser;
