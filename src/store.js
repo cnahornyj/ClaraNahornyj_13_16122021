c6a346222ed5a2ecf54953988b3735765bfc29a7
Clara <c.nahornyj@hotmail.fr>
2022-01-12 13:03:18 +0100

    Création du store avec le state initial, les action creators et le reducer + test fonctionnement des actions ok

--- src/store.js
+++ src/store.js
@@ -1,9 +1,32 @@
-// Logique redux
 import { createStore } from "redux";
 
 const initialState = {
-    isAuth: false
+  isLoggedIn: false,
+  currentUser: null,
+};
+
+// action creators
+
+const login = () => ({ type: "login" });
+
+function reducer(state = initialState, action) {
+  if (action.type === "login") {
+      return {
+          ...state,
+          isLoggedIn: !state.isLoggedIn,
+      }
+  }
+  return state;
 }
 
-// Mettre en place le store et les différentes actions
+// on crée le store avec le state et le reducer
+const store = createStore(reducer, initialState);
+store.dispatch({type: "login"})
+const state = store.getState();
+console.log(state);
 
+store.dispatch({type: "login"});
+const state1 = store.getState();
+console.log(state1);
+
+export default store;
