9f8a87bf2c7b5850b469e8c1e2a1a8a056ebdc0b
Clara <c.nahornyj@hotmail.fr>
2022-01-20 14:44:44 +0100

    Store fonctionnel

--- src/store.js
+++ src/store.js
@@ -1,32 +1,68 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
 
+// state initial
 const initialState = {
-  isLoggedIn: false,
-  currentUser: null,
+  loggedIn: false,
+  user: {},
 };
 
-// action creators
+// actions creator
+const setUser = (payload) => ({ type: "SET_USER", payload})
+export const logUserOut = () => ({type: "LOG_OUT"})
 
-const login = () => ({ type: "login" });
-
-function reducer(state = initialState, action) {
-  if (action.type === "login") {
+// reducer
+function userReducer(state = initialState, action) {
+  switch (action.type) {
+    case "SET_USER":
       return {
-          ...state,
-          isLoggedIn: !state.isLoggedIn,
-      }
+        ...state,
+        loggedIn: true,
+        user: { ...action.payload },
+      };
+    case "LOG_OUT":
+      localStorage.clear();
+      return {
+        ...state,
+        loggedIn: false,
+        user: {},
+      };
+    default:
+      return state;
   }
-  return state;
 }
 
-// on crée le store avec le state et le reducer
-const store = createStore(reducer, initialState);
-store.dispatch({type: "login"})
+export const store = createStore(userReducer, applyMiddleware(thunk));
+
+// store.dispatch({ type: "SET_USER" });
+// const state = store.getState();
+// console.log(state);
+// store.dispatch({ type: "LOG_OUT" });
+// const state1 = store.getState();
+// console.log(state1);
+
 const state = store.getState();
 console.log(state);
 
-store.dispatch({type: "login"});
-const state1 = store.getState();
-console.log(state1);
+const URL = 'localhost:3001/api/v1/';
+// const userInfos = {
+//   email: "tony@stark.com",
+//   password: "password123"
+// }
 
-export default store;
+export const fetchUser = (userInfos) => dispatch => {
+    fetch(URL+"login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(userInfos)
+    })
+    .then(res => res.json())
+    .then(data => {
+        localStorage.setItem("token", data.token)
+        dispatch(setUser(data.user))
+    })
+}
+
