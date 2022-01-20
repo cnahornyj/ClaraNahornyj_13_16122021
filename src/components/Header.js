1b36d55b278d19b35eded2277c783dfc48a9030e
Clara <c.nahornyj@hotmail.fr>
2022-01-20 14:45:34 +0100

    Utilisation useSelector pour la propriété loggedIn du store fonctionnel

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,8 @@ import logo from "../assets/argentBankLogo.png";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-/**  const userIsAuth = useSelector((state) => state.isAuth);
-  // on peut ensuite utiliser cette valeur dans le rendu
-  return <p>{userIsAuth ? "Link to login" : "Link to disconnect"}</p>; */
-
 function Header() {
-  const userIsAuth = useSelector((state) => state.isLoggedIn);
+  const loggedIn = useSelector((state) => state.loggedIn);
 
   return (
     <nav className="main-nav">
@@ -19,12 +15,11 @@ function Header() {
         />
         <h1 className="sr-only">Argent Bank</h1>
       </Link>
-      {userIsAuth ? (
+      {loggedIn ? (
         <div style={{display:"flex"}}>
           <i className="fas fa-user"></i>
           <p>user.firstname</p>
           <Link className="main-nav-item" to="/">
-          {/* onClick dispatch action login */}
             <i className="fas fa-sign-out-alt"></i>
             Sign Out
           </Link>
